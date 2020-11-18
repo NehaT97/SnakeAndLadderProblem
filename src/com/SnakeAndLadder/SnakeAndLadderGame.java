9b7bb0643013197638e05cd1dccbb0c39e4d6660
unknown <nehathakur764@gmail.com>
2020-11-19 00:12:18 +0530

    Welcome to Snake And Ladder Program

---
+++ src/com/SnakeAndLadder/SnakeAndLadderGame.java
@@ -0,0 +1,14 @@
+package com.SnakeAndLadder;
+
+public class SnakeAndLadderGame {
+
+        //uc1_single player at start position 0;
+        public static int position_of_player1=0;
+
+        public static void main(String args[])
+        {
+            System.out.println("WELCOME TO SNAKE AND LADDER GAME");
+        }
+
+
+}
